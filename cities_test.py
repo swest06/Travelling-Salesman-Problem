7069990627b4dd0e7253ace43b42fcca997f02a4
Sean West <swest06@dcs.bbk.ac.uk>
2018-03-22 16:31:57 +0000

    Edit test file

--- cities_test.py
+++ cities_test.py
@@ -2,13 +2,6 @@ import pytest
 from cities import*
 
 
-# def test_print_cities():
-#     road_map = [('Alabama', 'Montgomery', 32.361538, -86.279118), ('Alaska', 'Juneau', 58.301935, -134.41974), ('Arizona', 'Phoenix', 33.448457, -112.073844)]
-#     print_city = ['Montgomery 32.36 -86.28', 'Juneau 58.3 -134.42', 'Phoenix 33.45 -112.07']
-#
-#     assert print_city == print_cities(road_map)
-
-
 def test_compute_total_distance():
     co_ordinates = [("state A", "city A", 0, 0), ("state B", "city B", 0, 1), ("state C", "city C", 0, 2)]
     total = 276.18
